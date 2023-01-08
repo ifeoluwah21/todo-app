f960aa128dffba96ead08fd64202a8cc77d9f600
Ifeoulwah <spencerife.21@gmail.com>
2023-01-08 18:24:39 +0100

    auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import "./App.css";
-import Login from "./components/Auth/Login";
-import Registration from "./components/Auth/Registration";
-import Profile from "./components/Profile/Profile";
-import Button from "./components/UIs/Button";
-import HomeSection from "./components/home/HomeSection";
-import img from "./assets/avatar.png";
-import Clock from "./components/UIs/Clock";
-import clock from "./assets/clock.png";
-import Wrapper from "./components/UIs/Wrapper";
-import TodoList from "./components/Todos/TodoList";
+import './App.css';
+import Login from './components/Auth/Login';
+import Registration from './components/Auth/Registration';
+import Profile from './components/Profile/Profile';
+import Button from './components/UIs/Button';
+import HomeSection from './components/home/HomeSection';
+import img from './assets/avatar.png';
+import Clock from './components/UIs/Clock';
+import clock from './assets/clock.png';
+import Wrapper from './components/UIs/Wrapper';
+import TodoList from './components/Todos/TodoList';
 import {
 	Navigate,
 	Route,
 	RouterProvider,
 	createBrowserRouter,
 	createRoutesFromElements,
-} from "react-router-dom";
-import Home from "./Pages/Home";
-import Auth from "./Pages/Register";
-import RootLayout from "./Pages/RootLayout";
-import UserTodos from "./Pages/UserTodos";
-import Register from "./Pages/Register";
+} from 'react-router-dom';
+import Home from './Pages/Home';
+import Auth from './Pages/Register';
+import RootLayout from './Pages/RootLayout';
+import UserTodos from './Pages/UserTodos';
+import Register from './Pages/Register';
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
@@ -28,10 +28,10 @@ const router = createBrowserRouter(
 			path="/"
 			element={<RootLayout />}>
 			<Route
-				path={"/home"}
+				path={'/home'}
 				element={
 					<Navigate
-						to={"/"}
+						to={'/'}
 						replace={true}
 					/>
 				}
@@ -49,7 +49,7 @@ const router = createBrowserRouter(
 				element={<Login />}
 			/>
 			<Route
-				path="/user"
+				path="/user/:userId"
 				element={<UserTodos />}
 			/>
 		</Route>
---
+++ src/api/firebase.js
@@ -0,0 +1,87 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import {
+	createUserWithEmailAndPassword,
+	getAuth,
+	signInWithEmailAndPassword,
+	onAuthStateChanged,
+	signInWithPopup,
+	signInWithRedirect,
+	getRedirectResult,
+} from 'firebase/auth';
+import { GoogleAuthProvider } from 'firebase/auth';
+
+export const provider = new GoogleAuthProvider();
+
+provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+provider.setCustomParameters({
+	login_hint: 'user@example.com',
+});
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyBk79MnTPIho9qvkSNRn_7MtqcCjoyIRPg',
+	authDomain: 'todo-app-e449b.firebaseapp.com',
+	projectId: 'todo-app-e449b',
+	storageBucket: 'todo-app-e449b.appspot.com',
+	messagingSenderId: '944211147263',
+	appId: '1:944211147263:web:550c4de7a9b60a5f9a4fca',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
+auth.languageCode = 'it';
+
+export async function signIn(email, password) {
+	signInWithEmailAndPassword(auth, email, password)
+		.then((userCredential) => {
+			// Signed in
+			const user = userCredential.user;
+			console.log(user);
+			// ...
+		})
+		.catch((error) => {
+			const errorCode = error.code;
+			const errorMessage = error.message;
+			console.log(error);
+		});
+}
+
+onAuthStateChanged(auth, (user) => {
+	if (user) {
+		// User is signed in, see docs for a list of available properties
+		// https://firebase.google.com/docs/reference/js/firebase.User
+		const uid = user.uid;
+		// ...
+	} else {
+		// User is signed out
+		// ...
+	}
+});
+export function sign() {
+	signInWithPopup(auth, provider)
+		.then((result) => {
+			// This gives you a Google Access Token. You can use it to access the Google API.
+			const credential = GoogleAuthProvider.credentialFromResult(result);
+			const token = credential.accessToken;
+			// The signed-in user info.
+			const user = result.user;
+			console.log(token, user);
+			// ...
+		})
+		.catch((error) => {
+			// Handle Errors here.
+			const errorCode = error.code;
+			const errorMessage = error.message;
+			// The email of the user's account used.
+			const email = error.customData.email;
+			// The AuthCredential type that was used.
+			const credential = GoogleAuthProvider.credentialFromError(error);
+			// ...
+		});
+}
