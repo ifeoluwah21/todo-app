e232a21f7f59ff1b9c956859d6e7f2fd259df2e5
Ifeoluwah <69403530+ifeoluwah21@users.noreply.github.com>
2023-02-09 10:09:13 +0100

    Dynamic Greeting feature

--- src/components/UIs/Clock.tsx
+++ src/components/UIs/Clock.tsx
@@ -3,6 +3,14 @@ import React from "react";
 const Clock: React.FC<{
 	img: string | undefined;
 }> = (props) => {
+	const getTime = () => {
+		let currentTime: Date = new Date();
+		let currentHour: number = currentTime.getHours();
+		if (currentHour >= 3 && currentHour < 12) return "Good Morning";
+		else if (currentHour >= 12 && currentHour < 4) return `Good Afternoon`;
+		else if (currentHour >= 4 && currentHour > 7) return "Good Evening";
+		else return "Good Night";
+	};
 	return (
 		<figure className="w-full flex flex-col-reverse sm:flex-col gap-2 py-8 px-4 sm:px-8 justify-center items-center">
 			<img
@@ -12,7 +20,7 @@ const Clock: React.FC<{
 				width={"96"}
 			/>
 			<figcaption className="text-base text-blue-700 tracking-wide font-bold self-end sm:self-center ">
-				Good afternoon
+				{getTime()}
 			</figcaption>
 		</figure>
 	);
