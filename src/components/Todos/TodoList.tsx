83ddf14cc2bee0815ccf6188ad95e4d512ab7630
Ifeoulwah <spencerife.21@gmail.com>
2023-02-27 15:17:53 +0100

    implemented firestore db

--- src/components/Todos/TodoList.tsx
+++ src/components/Todos/TodoList.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Todo } from '../../models/Todo';
 import TodoItem from './TodoItem';
 import { useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import { listVariant, taskContainerVariant } from '../../api/animate';
+import { authContext } from '../../store/AuthContext';
 
 const TodoList: React.FC = () => {
 	const navigate = useNavigate();
+	const { user } = useContext(authContext);
 	const addHandler = (event: React.MouseEvent) => {
-		navigate('/addTodo');
+		navigate(`/user/${user!.uid}/addTodo`);
 	};
 	return (
 		<motion.section
@@ -22,20 +24,22 @@ const TodoList: React.FC = () => {
 			<div className="bg-white pb-10 pt-6 px-4 rounded-xl mb-12 max-h-80 overflow-y-scroll hide-scrollbar w-80 sm:w-96 max-w-sm mx-auto">
 				<h2 className="font-bold text-xl text-left text-blue-700 pb-5 flex flex-row justify-between w-full">
 					<span>Daily tasks</span>
-					<button onClick={addHandler}>
-						<svg
-							className="w-8 h-8 font-bold text-blue-700"
-							fill="none"
-							stroke="currentColor"
-							viewBox="0 0 24 24"
-							xmlns="http://www.w3.org/2000/svg">
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth="2"
-								d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
-						</svg>
-					</button>
+					{user && user.uid && (
+						<button onClick={addHandler}>
+							<svg
+								className="w-8 h-8 font-bold text-blue-700"
+								fill="none"
+								stroke="currentColor"
+								viewBox="0 0 24 24"
+								xmlns="http://www.w3.org/2000/svg">
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth="2"
+									d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+							</svg>
+						</button>
+					)}
 				</h2>
 				<TodoItem />
 			</div>
